79880b115b9379ab840f265b5dcb37f48d2fea77
Collesss <sergey_lesikov@mail.ru>
2026-08-19 00:06:23 +0300

    Add filter to repository get method.

--- Infrastructure.Persistence.SQLite/Implementations/SwitchRepository.cs
+++ Infrastructure.Persistence.SQLite/Implementations/SwitchRepository.cs
@@ -3,9 +3,12 @@ using Application.Repository.Exceptions.Enums;
 using Application.Repository.Interfaces;
 using Application.Repository.Models;
 using Infrastructure.Persistence.SQLite.Models;
+using LinqKit;
 using MapsterMapper;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 
 namespace Infrastructure.Persistence.SQLite.Implementations
 {
@@ -36,6 +39,10 @@ namespace Infrastructure.Persistence.SQLite.Implementations
             {
                 throw new RepositoryException(RepositoryErrorCode.SwitchAddIpAlreadyExist, e);
             }
+            catch (Exception e)
+            {
+                throw new RepositoryException(RepositoryErrorCode.Unknow, e);
+            }
         }
 
         public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
@@ -50,10 +57,18 @@ namespace Infrastructure.Persistence.SQLite.Implementations
             {
                 throw new RepositoryException(RepositoryErrorCode.SwitchDeleteNotFound, e);
             }
+            catch(Exception e)
+            {
+                throw new RepositoryException(RepositoryErrorCode.Unknow, e);
+            }
         }
 
         public Task<SwitchesListDto> Get(GetSwitchesListDto getDto, CancellationToken cancellationToken = default)
         {
+            var filter = getDto.GetType().GetProperties()
+                .Where(prop => prop.Name.StartsWith("SearchBy") && prop.GetValue(getDto) is not null)
+                .Aggregate(PredicateBuilder.New<SwitchDbEntity>(true), (seed, prop) => seed.And(dbEnt => EF.Functions.Like($"{prop.Name}", prop.GetValue(getDto).ToString())));
+
 
 
             throw new NotImplementedException();
@@ -62,7 +77,20 @@ namespace Infrastructure.Persistence.SQLite.Implementations
         public async Task<IEnumerable<SwitchDto>> GetAll(CancellationToken cancellationToken = default) =>
             _mapper.Map<IEnumerable<SwitchDbEntity>, IEnumerable<SwitchDto>>(await _dbContext.Switches.ToListAsync(cancellationToken));
 
-        public async Task<SwitchDto> GetById(int id, CancellationToken cancellationToken = default) =>
-            _mapper.Map<SwitchDbEntity, SwitchDto>(await _dbContext.Switches.FindAsync([id], cancellationToken) ?? throw new RepositoryException(RepositoryErrorCode.SwitchGetByIdNotFound));
+        public async Task<SwitchDto> GetById(int id, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return _mapper.Map<SwitchDbEntity, SwitchDto>(await _dbContext.Switches.FindAsync([id], cancellationToken) ?? throw new RepositoryException(RepositoryErrorCode.SwitchGetByIdNotFound));
+            }
+            catch(RepositoryException)
+            {
+                throw;
+            }
+            catch(Exception e)
+            {
+                throw new RepositoryException(RepositoryErrorCode.Unknow, e);
+            }
+        }
     }
 }
